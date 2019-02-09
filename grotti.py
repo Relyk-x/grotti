6971d0332a86bae4d2bfa592db63831c2f79d7cc
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2019-02-09 17:02:22 +0800

    Update grotti.py

--- grotti.py
+++ grotti.py
@@ -18,7 +18,7 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def invite(ctx, user: discord.Member):
-    invite = await bot.say(f"**Invite**\n{user.name} a new server has been partnered! You have been invited to join {servername}\n\n<:discord:535748146761039872> Discord Server: https://discord.gg/8GNRBsr")
-    await bot.send_message(user, f"{invite}")
+    partner = await bot.say(f"**Invite**\n{user.name} a new server has been partnered! You have been invited to join {servername}\n\n<:discord:535748146761039872> Discord Server: https://discord.gg/8GNRBsr")
+    await bot.send_message(user, f"{partner}")
 
 bot.run(os.getenv("BOT_TOKEN"))
