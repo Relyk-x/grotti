31debeb4bc7c68cee7107c515df48978bfaf5452
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2019-02-09 16:14:44 +0800

    Update grotti.py

--- grotti.py
+++ grotti.py
@@ -9,16 +9,17 @@ servername = "ИEVER WΛVE"
 
 @bot.event
 async def on_ready():
-	print ("------------------------------------")
-	print (f"Bot Name: {bot.user.name}")
-	print (f"Bot ID: {bot.user.id}")
-	print (f"Discord Version: {discord.__version__}")
-	print ("Ready to invite new members...")
-	print ("------------------------------------")
+    print ("------------------------------------")
+    print (f"Bot Name: {bot.user.name}")
+    print (f"Bot ID: {bot.user.id}")
+    print (f"Discord Version: {discord.__version__}")
+    print ("Ready to invite new members...")
+    print ("------------------------------------")
 
 @bot.command(pass_context=True)
-async def invite(ctx, user: discord.Id):
+async def invite(ctx, user: discord.Member):
+    userid = discord.utils.get(bot.get_all_members(), id=f"{user}")
     invite = await bot.say(f"**Invite**\n{user.name} a new server has been partnered! You have been invited to join {servername}\n\n<:discord:535748146761039872> Discord Server: https://discord.gg/SX93MmJ\n")
-    await bot.send_message(user, f"{invite}")
+    await bot.send_message(userid, f"{invite}")
 
 bot.run(os.getenv("BOT_TOKEN"))
