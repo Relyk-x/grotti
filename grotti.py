d12d1ed0a332cd580cb635495fa5562669d021ad
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2019-02-10 18:16:21 +0800

    Update grotti.py

--- grotti.py
+++ grotti.py
@@ -5,31 +5,37 @@ import asyncio
 import os
 
 bot = commands.Bot(command_prefix='&')
+servername = "Lewd Lounge +18"
+
+
 
 @bot.event
 async def on_ready():
+    servers = list(bot.servers)
+    status = f"over {str(len(bot.servers))} servers"
     print ("------------------------------------")
     print (f"Bot Name: {bot.user.name}")
     print (f"Bot ID: {bot.user.id}")
     print (f"Discord Version: {discord.__version__}")
     print ("Ready to invite new members...")
     print ("------------------------------------")
+    await bot.change_presence(game=discord.Game(name=status,type=3))
 
-#@bot.command(pass_context=True)
-#async def invite(ctx, user: discord.Member):
-    #partner = f"**Invite**\n{user.name} a new server has been partnered! You have been invited to join {servername}\n\n<:discord:535748146761039872> Discord Server: https://discord.gg/8GNRBsr"
-    #await bot.send_message(user, f"{partner}")
+@bot.command(pass_context=True)
+async def invite(ctx, user: discord.Member):
+    partner = f"**Invite**\n{user.name} a new server has been partnered! You have been invited to join {servername}\n\n<:discord:535748146761039872> Discord Server: https://discord.gg/8GNRBsr"
+    await bot.send_message(user, f"{partner}")
 
-@bot.command(pass_context = True)
-async def member_id(ctx):
-    memb = list(bot.members)
-    for x in range(len(memb)):
-        await bot.say(memb[x-1].id)
+#@bot.command(pass_context = True)
+#async def member_id(ctx):
+    #memb = list(bot.members)
+    #for x in range(len(memb)):
+        #await bot.say(memb[x-1].id)
 
-@bot.command(pass_context = True)
-async def invite(ctx, userToInvite):
-    inviteLinq = await bot.create_invite(destination = ctx.message.channel, xkcd = True, max_uses = 1)
-    target_user = await bot.get_user_info(userToInvite)
-    await bot.send_message(target_user, f"**Invite**\nA new server has been partnered! You are invited to join...\n{inviteLinq}")
+#@bot.command(pass_context = True)
+#async def invite(ctx, userToInvite):
+    #inviteLinq = await bot.create_invite(destination = ctx.message.channel, xkcd = True, max_uses = 1)
+    #target_user = await bot.get_user_info(userToInvite)
+    #await bot.send_message(target_user, f"**Invite**\nA new server has been partnered! You are invited to join...\n{inviteLinq}")
 
 bot.run(os.getenv("BOT_TOKEN"))
